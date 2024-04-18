003aca40c43ad7d90f89830b4eae09608f414c52
FlanitzerMoritz <125306682+FlanitzerMoritz@users.noreply.github.com>
2024-04-18 12:06:56 +0200

    Javascript für die Fragen 6,7,8 geschrieben und alle vorherigen Fragen auf die neue Tabelle angepasst

--- CrosswordMerged/js/script.js
+++ CrosswordMerged/js/script.js
@@ -32,50 +32,51 @@ function sumbit1() {
   if (userInput == words[0]) {
     document.getElementById('Q1').style.textDecoration = 'line-through';
 
-    document.getElementById('a2').innerHTML = letters0[0];
-    document.getElementById('b2').innerHTML = letters0[1];
-    document.getElementById('c2').innerHTML = letters0[2];
-    document.getElementById('d2').innerHTML = letters0[3];
-    document.getElementById('e2').innerHTML = letters0[4];
+    document.getElementById('f3').innerHTML = letters0[0];
+    document.getElementById('g3').innerHTML = letters0[1];
+    document.getElementById('h3').innerHTML = letters0[2];
+    document.getElementById('i3').innerHTML = letters0[3];
+    document.getElementById('j3').innerHTML = letters0[4];
   } else if (userInput == words[1]) {
     document.getElementById('Q2').style.textDecoration = 'line-through';
 
-    document.getElementById('e2').innerHTML = letters1[0];
-    document.getElementById('e3').innerHTML = letters1[1];
-    document.getElementById('e4').innerHTML = letters1[2];
-    document.getElementById('e5').innerHTML = letters1[3];
-    document.getElementById('e6').innerHTML = letters1[4];
+    document.getElementById('j3').innerHTML = letters1[0];
+    document.getElementById('j4').innerHTML = letters1[1];
+    document.getElementById('j5').innerHTML = letters1[2];
+    document.getElementById('j6').innerHTML = letters1[3];
+    document.getElementById('j7').innerHTML = letters1[4];
   } else if (userInput == words[2]) {
     document.getElementById('Q3').style.textDecoration = 'line-through';
 
-    document.getElementById('e4').innerHTML = letters2[0];
-    document.getElementById('d4').innerHTML = letters2[1];
-    document.getElementById('c4').innerHTML = letters2[2];
-    document.getElementById('b4').innerHTML = letters2[3];
-    document.getElementById('a4').innerHTML = letters2[4];
+    document.getElementById('j4').innerHTML = letters2[0];
+    document.getElementById('k4').innerHTML = letters2[1];
+    document.getElementById('l4').innerHTML = letters2[2];
+    document.getElementById('m4').innerHTML = letters2[3];
+    document.getElementById('n4').innerHTML = letters2[4];
   } else if (userInput == words[3]) {
     document.getElementById('Q4').style.textDecoration = 'line-through';
 
-    document.getElementById('e5').innerHTML = letters3[0];
-    document.getElementById('f5').innerHTML = letters3[1];
-    document.getElementById('g5').innerHTML = letters3[2];
+    document.getElementById('l7').innerHTML = letters3[0];
+    document.getElementById('m7').innerHTML = letters3[1];
+    document.getElementById('n7').innerHTML = letters3[2];
   } else if (userInput == words[4]) {
     document.getElementById('Q5').style.textDecoration = 'line-through';
 
-    document.getElementById('c7').innerHTML = letters4[0]
-    document.getElementById('d7').innerHTML = letters4[1]
-    document.getElementById('e7').innerHTML = letters4[2]
-    document.getElementById('f7').innerHTML = letters4[3]
-    document.getElementById('g7').innerHTML = letters4[4]
-    document.getElementById('h7').innerHTML = letters4[5]
+    document.getElementById('b6').innerHTML = letters4[0]
+    document.getElementById('c6').innerHTML = letters4[1]
+    document.getElementById('d6').innerHTML = letters4[2]
+    document.getElementById('e6').innerHTML = letters4[3]
+    document.getElementById('f6').innerHTML = letters4[4]
+    document.getElementById('g6').innerHTML = letters4[5]
   } else if (userInput == words[5]) {
     document.getElementById('Q2').style.textDecoration = 'line-through';
 
-    document.getElementById('e2').innerHTML = letters1[0];
-    document.getElementById('e3').innerHTML = letters1[1];
-    document.getElementById('e4').innerHTML = letters1[2];
-    document.getElementById('e5').innerHTML = letters1[3];
-    document.getElementById('e6').innerHTML = letters1[4];
+    document.getElementById('l3').innerHTML = letters5[0];
+    document.getElementById('l4').innerHTML = letters5[1];
+    document.getElementById('l5').innerHTML = letters5[2];
+    document.getElementById('l6').innerHTML = letters5[3];
+    document.getElementById('l7').innerHTML = letters5[4];
+    document.getElementById('l8').innerHTML = letters5[5];
   }
 
   // Change style according to input
@@ -84,7 +85,10 @@ function sumbit1() {
     userInput != words[1] &&
     userInput != words[2] &&
     userInput != words[3] &&
-    userInput != words[4]
+    userInput != words[4] &&
+    userInput != words[5] &&
+    userInput != words[6] &&
+    userInput != words[7]
   ) {
     document.getElementById('submissionText').style.backgroundColor = 'red';
   } else {
@@ -119,22 +123,31 @@ let shownQ2 = false;
 let shownQ3 = false;
 let shownQ4 = false;
 let shownQ5 = false;
+let shownQ6 = false;
+let shownQ7 = false;
+let shownQ8 = false;
 
 //Div Box Q1
-document.getElementById('a2').addEventListener('click', () => {
+document.getElementById('f3').addEventListener('click', () => {
   if (!shownQ1) {
     document.getElementById("hiddenBoxQ1").style.display = "block";
     shownQ1 = true;
     console.log('Div Q1 wird angezeigt')
-    //Hide other Question Divs
+    //Hide other Question Divs  
     document.getElementById("hiddenBoxQ2").style.display = "none";
     document.getElementById("hiddenBoxQ3").style.display = "none";
     document.getElementById("hiddenBoxQ4").style.display = "none";
     document.getElementById("hiddenBoxQ5").style.display = "none";
+    document.getElementById("hiddenBoxQ6").style.display = "none";
+    document.getElementById("hiddenBoxQ7").style.display = "none";
+    document.getElementById("hiddenBoxQ8").style.display = "none";
     shownQ2 = false;
     shownQ3 = false;
     shownQ4 = false;
     shownQ5 = false;
+    shownQ6 = false;
+    shownQ7 = false;
+    shownQ8 = false;
 
   }
   else if (shownQ1) {
@@ -145,7 +158,7 @@ document.getElementById('a2').addEventListener('click', () => {
 });
 
 //Div Box Q2
-document.getElementById('e2').addEventListener('click', () => {
+document.getElementById('j3').addEventListener('click', () => {
   if (!shownQ2) {
     document.getElementById("hiddenBoxQ2").style.display = "block";
     shownQ2 = true;
@@ -153,10 +166,16 @@ document.getElementById('e2').addEventListener('click', () => {
     document.getElementById("hiddenBoxQ3").style.display = "none";
     document.getElementById("hiddenBoxQ4").style.display = "none";
     document.getElementById("hiddenBoxQ5").style.display = "none";
+    document.getElementById("hiddenBoxQ6").style.display = "none";
+    document.getElementById("hiddenBoxQ7").style.display = "none";
+    document.getElementById("hiddenBoxQ8").style.display = "none";
     shownQ1 = false;
     shownQ3 = false;
     shownQ4 = false;
     shownQ5 = false;
+    shownQ6 = false;
+    shownQ7 = false;
+    shownQ8 = false;
     console.log('Div Q2 wird angezeigt')
 
   }
@@ -168,7 +187,7 @@ document.getElementById('e2').addEventListener('click', () => {
 });
 
 //Div Box Q3
-document.getElementById('e4').addEventListener('click', () => {
+document.getElementById('j4').addEventListener('click', () => {
   if (!shownQ3) {
     document.getElementById("hiddenBoxQ3").style.display = "block";
     shownQ3 = true;
@@ -176,10 +195,16 @@ document.getElementById('e4').addEventListener('click', () => {
     document.getElementById("hiddenBoxQ2").style.display = "none";
     document.getElementById("hiddenBoxQ4").style.display = "none";
     document.getElementById("hiddenBoxQ5").style.display = "none";
+    document.getElementById("hiddenBoxQ6").style.display = "none";
+    document.getElementById("hiddenBoxQ7").style.display = "none";
+    document.getElementById("hiddenBoxQ8").style.display = "none";
     shownQ1 = false;
     shownQ2 = false;
     shownQ4 = false;
     shownQ5 = false;
+    shownQ6 = false;
+    shownQ7 = false;
+    shownQ8 = false;
     console.log('Div Q3 wird angezeigt')
 
   }
@@ -191,7 +216,7 @@ document.getElementById('e4').addEventListener('click', () => {
 });
 
 //Div Box Q4
-document.getElementById('e5').addEventListener('click', () => {
+document.getElementById('l7').addEventListener('click', () => {
   if (!shownQ4) {
     document.getElementById("hiddenBoxQ4").style.display = "block";
     shownQ4 = true;
@@ -199,10 +224,16 @@ document.getElementById('e5').addEventListener('click', () => {
     document.getElementById("hiddenBoxQ2").style.display = "none";
     document.getElementById("hiddenBoxQ3").style.display = "none";
     document.getElementById("hiddenBoxQ5").style.display = "none";
+    document.getElementById("hiddenBoxQ6").style.display = "none";
+    document.getElementById("hiddenBoxQ7").style.display = "none";
+    document.getElementById("hiddenBoxQ8").style.display = "none";
     shownQ1 = false;
     shownQ2 = false;
     shownQ3 = false;
     shownQ5 = false;
+    shownQ6 = false;
+    shownQ7 = false;
+    shownQ8 = false;
     console.log('Div Q4 wird angezeigt')
 
   }
@@ -214,7 +245,7 @@ document.getElementById('e5').addEventListener('click', () => {
 });
 
 //Div Box Q5
-document.getElementById('c7').addEventListener('click', () => {
+document.getElementById('b6').addEventListener('click', () => {
   if (!shownQ5) {
     document.getElementById("hiddenBoxQ5").style.display = "block";
     shownQ5 = true;
@@ -222,10 +253,16 @@ document.getElementById('c7').addEventListener('click', () => {
     document.getElementById("hiddenBoxQ2").style.display = "none";
     document.getElementById("hiddenBoxQ3").style.display = "none";
     document.getElementById("hiddenBoxQ4").style.display = "none";
+    document.getElementById("hiddenBoxQ6").style.display = "none";
+    document.getElementById("hiddenBoxQ7").style.display = "none";
+    document.getElementById("hiddenBoxQ8").style.display = "none";
     shownQ1 = false;
     shownQ2 = false;
     shownQ3 = false;
     shownQ4 = false;
+    shownQ6 = false;
+    shownQ7 = false;
+    shownQ8 = false;
     console.log('Div Q5 wird angezeigt')
 
   }
@@ -233,6 +270,93 @@ document.getElementById('c7').addEventListener('click', () => {
     document.getElementById("hiddenBoxQ5").style.display = "none";
     console.log('Div Q5 wird nichtmehr angezeigt')
     shownQ5 = false;
+  }
+});
+
+//Div Box Q6
+document.getElementById('l3').addEventListener('click', () => {
+  if (!shownQ6) {
+    document.getElementById("hiddenBoxQ6").style.display = "block";
+    shownQ6 = true;
+    document.getElementById("hiddenBoxQ1").style.display = "none";
+    document.getElementById("hiddenBoxQ2").style.display = "none";
+    document.getElementById("hiddenBoxQ3").style.display = "none";
+    document.getElementById("hiddenBoxQ4").style.display = "none";
+    document.getElementById("hiddenBoxQ5").style.display = "none";
+    document.getElementById("hiddenBoxQ7").style.display = "none";
+    document.getElementById("hiddenBoxQ8").style.display = "none";
+    shownQ1 = false;
+    shownQ2 = false;
+    shownQ3 = false;
+    shownQ4 = false;
+    shownQ5 = false;    
+    shownQ7 = false;
+    shownQ8 = false;
+    console.log('Div Q6 wird angezeigt')
+
+  }
+  else if (shownQ6) {
+    document.getElementById("hiddenBoxQ6").style.display = "none";
+    console.log('Div Q6 wird nichtmehr angezeigt')
+    shownQ6 = false;
+  }
+});
+
+//Div Box Q7
+document.getElementById('g3').addEventListener('click', () => {
+  if (!shownQ7) {
+    document.getElementById("hiddenBoxQ7").style.display = "block";
+    shownQ7 = true;
+    document.getElementById("hiddenBoxQ1").style.display = "none";
+    document.getElementById("hiddenBoxQ2").style.display = "none";
+    document.getElementById("hiddenBoxQ3").style.display = "none";
+    document.getElementById("hiddenBoxQ4").style.display = "none";
+    document.getElementById("hiddenBoxQ5").style.display = "none";
+    document.getElementById("hiddenBoxQ6").style.display = "none";
+    document.getElementById("hiddenBoxQ8").style.display = "none";
+    shownQ1 = false;
+    shownQ2 = false;
+    shownQ3 = false;
+    shownQ4 = false;
+    shownQ5 = false;    
+    shownQ6 = false;
+    shownQ8 = false;
+    console.log('Div Q7 wird angezeigt')
+
+  }
+  else if (shownQ7) {
+    document.getElementById("hiddenBoxQ7").style.display = "none";
+    console.log('Div Q7 wird nichtmehr angezeigt')
+    shownQ7 = false;
+  }
+});
+
+//Div Box Q8
+document.getElementById('b8').addEventListener('click', () => {
+  if (!shownQ8) {
+    document.getElementById("hiddenBoxQ8").style.display = "block";
+    shownQ8 = true;
+    document.getElementById("hiddenBoxQ1").style.display = "none";
+    document.getElementById("hiddenBoxQ2").style.display = "none";
+    document.getElementById("hiddenBoxQ3").style.display = "none";
+    document.getElementById("hiddenBoxQ4").style.display = "none";
+    document.getElementById("hiddenBoxQ5").style.display = "none";
+    document.getElementById("hiddenBoxQ6").style.display = "none";
+    document.getElementById("hiddenBoxQ7").style.display = "none";
+    shownQ1 = false;
+    shownQ2 = false;
+    shownQ3 = false;
+    shownQ4 = false;
+    shownQ5 = false;    
+    shownQ6 = false;
+    shownQ7 = false;
+    console.log('Div Q8 wird angezeigt')
+
+  }
+  else if (shownQ8) {
+    document.getElementById("hiddenBoxQ8").style.display = "none";
+    console.log('Div Q8 wird nichtmehr angezeigt')
+    shownQ8 = false;
   }
 });
 
